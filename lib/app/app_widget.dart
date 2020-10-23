86982d4299775790ade2ce90b9676868cb840c70
FelipeHbsis <felipe.msana@gmail.com>
2020-10-23 17:47:47 -0300

    Controle de Temas

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,37 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gofast/app/app_controller.dart';
 import 'package:flutter_gofast/app/core/intercionalization/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      initialRoute: '/',
-      navigatorKey: Modular.navigatorKey,
-      onGenerateRoute: Modular.generateRoute,
-      supportedLocales: [
-        Locale('en', 'US'),
-        Locale('pt', 'BR'),
-      ],
-      localizationsDelegates: [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      localeResolutionCallback: (locale, supportdLocales) {
-        for (var supportedLocale in supportdLocales) {
-          if (supportedLocale.countryCode == locale.languageCode &&
-              supportedLocale.countryCode == locale.countryCode) {
-            return supportedLocale;
+    return Observer(builder: (context) {
+      return MaterialApp(
+        title: 'Flutter Demo',
+        initialRoute: '/',
+        theme: Modular.get<AppController>().themeApp.getTheme(),
+        navigatorKey: Modular.navigatorKey,
+        onGenerateRoute: Modular.generateRoute,
+        supportedLocales: [
+          Locale('en', 'US'),
+          Locale('pt', 'BR'),
+        ],
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        localeResolutionCallback: (locale, supportdLocales) {
+          for (var supportedLocale in supportdLocales) {
+            if (supportedLocale.countryCode == locale.languageCode &&
+                supportedLocale.countryCode == locale.countryCode) {
+              return supportedLocale;
+            }
           }
-        }
-        return supportdLocales.first;
-      },
-    );
+          return supportdLocales.first;
+        },
+      );
+    });
   }
 }
